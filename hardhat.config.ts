421295f100a5047cf3d645f270845ba13ce4fdd5
paul <paul@unifra.io>
2024-06-19 11:33:03 +0800

    refactor: update sample to use merlin testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,43 +1,44 @@
 import * as dotenv from "dotenv";
 
 import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-verify";
+
+import type { HardhatUserConfig } from "hardhat/config";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  networks: {
-    scrollTestnet: {
-      url: process.env.SCROLL_TESTNET_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  // npx hardhat verify --network scrollTestnet 0x95ad51f4406bf2AF31e3A2e2d75262EE19432261 123
-  etherscan: {
-    apiKey: {
-      scrollTestnet: "abc",
-    },
-    customChains: [
-      {
-        network: "scrollTestnet",
-        chainId: 534351,
-        urls: {
-          apiURL: "https://scroll-sepolia.l2scan.co/api/contract",
-          browserURL: "https://scroll-sepolia.l2scan.co/",
-        },
-      },
-    ],
-  },
+	solidity: {
+		version: "0.8.9",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	networks: {
+		merlinTestnet: {
+			url: process.env.RPC || "",
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
+	etherscan: {
+		apiKey: {
+			merlinTestnet: "no-need-apikey",
+		},
+		customChains: [
+			{
+				network: "merlinTestnet",
+				chainId: 686868,
+				urls: {
+					apiURL: "https://testnet-scan.merlinchain.io/api",
+					browserURL: "https://testnet-scan.merlinchain.io/",
+				},
+			},
+		],
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,28 +1,26 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("0.00000001");
+	const lockedAmount = ethers.parseEther("0.00000001");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	const Lock = await ethers.getContractFactory("Lock");
+	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 0.00000001 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-  console.log(
-    `Block explorer URL: https://scroll-sepolia.l2scan.co/address/${lock.address}`
-  );
+	console.log(
+		`Lock with 0.00000001 ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`,
+	);
+	console.log(
+		`Block explorer URL: https://testnet-scan.merlinchain.io/address/${lock.target}`,
+	);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
